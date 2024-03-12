632bc76608225973cb643b6e1c0e7bb556c964fe
psrom <97152657+psrom@users.noreply.github.com>
2024-03-12 21:02:13 +0900

    Title: Cycle, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240312/Cycle/cycle.py
+++ 240312/Cycle/cycle.py
@@ -1,14 +1,16 @@
 N, P = map(int, input().split())
 stack = []
 temp = N
+idx = 0
 
 while True:
     temp *= N
     temp %= P
     
     if temp in stack:
+        idx = stack.index(temp)
         break
     else:
         stack.append(temp)
-    
-print(len(stack))
+
+print(len(stack) - idx)
