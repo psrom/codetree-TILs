ed1cbfd5c33ae2e712e5f69713b485496cb0a3a8
psrom <97152657+psrom@users.noreply.github.com>
2024-03-15 17:27:00 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 153ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240315/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,11 @@
+n = int(input())
+lst = list(map(int, input().split()))
+
+cnt = 0
+for i in range(n):
+    if lst[i] == 2:
+        cnt += 1
+
+    if cnt == 3:
+        print(i + 1)
+        break
