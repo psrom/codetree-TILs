9c0ee6dc61ed51288d4c733a3383a007afe044ad
psrom <97152657+psrom@users.noreply.github.com>
2024-03-19 13:53:41 +0900

    Title: 미는 횟수, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240319/미는 횟수/number-of-pushes.py
+++ 240319/미는 횟수/number-of-pushes.py
@@ -2,12 +2,12 @@ A = input()
 B = input()
 
 check = False
-for i in range(len(A) - 1):
+for i in range(len(A)):
     A = A[1:] + A[0]
     if A == B:
-        print(i + 1)
         check = True
+        print(i + 1)
         break
-        
+
 if not check:
     print(-1)
