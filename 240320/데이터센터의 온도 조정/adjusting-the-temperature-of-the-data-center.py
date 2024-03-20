833d38b9b2702b73f9a2484f32981f3585a73282
psrom <97152657+psrom@users.noreply.github.com>
2024-03-20 23:19:01 +0900

    Title: 데이터센터의 온도 조정, Time: 212ms, Memory: 26MB, Status: Passed - CodeTree

--- 240320/데이터센터의 온도 조정/adjusting-the-temperature-of-the-data-center.py
+++ 240320/데이터센터의 온도 조정/adjusting-the-temperature-of-the-data-center.py
@@ -1,19 +1,27 @@
+MAX_T = 1_000_000_001
 N, C, G, H = map(int, input().split())
-temp = list(list(map(int, input().split())) for _ in range(N))
 
-min_temp = min(map(min, temp)) - 1
-max_temp = max(map(max, temp)) + 1
+A, B = [MAX_T], [MAX_T]
 
-answer = 0
-for i in range(min_temp, max_temp + 1):
-    s = 0
-    for ta, tb in temp:
-        if i < ta:
-            s += C
-        elif ta <= i <= tb:
-            s += G
-        else:
-            s += H 
-    answer = max(answer, s)
+for _ in range(N):
+    ta, tb = map(int, input().split())
+    A.append(ta)
+    B.append(tb)
 
+A.sort()
+B.sort()
+
+i, j = 0, 0
+current_work = N * C
+answer = N * C
+
+while i < N or j < N:
+    if A[i] <= B[j]:
+        current_work += G - C
+        i += 1
+    else:
+        current_work += H - G
+        j += 1
+    if current_work > answer:
+        answer = current_work
 print(answer)
