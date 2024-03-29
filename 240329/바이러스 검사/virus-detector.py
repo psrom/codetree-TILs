fd0e4846d0492f47ad1fcc8e069a35c979059307
psrom <97152657+psrom@users.noreply.github.com>
2024-03-29 18:27:45 +0900

    Title: 바이러스 검사, Time: 370ms, Memory: 122MB, Status: Passed - CodeTree

--- 240329/바이러스 검사/virus-detector.py
+++ 240329/바이러스 검사/virus-detector.py
@@ -2,12 +2,16 @@ n = int(input())
 customers = list(map(int, input().split()))
 leader, fellow = map(int, input().split())
 
-answer = n
-customers = [i - leader for i in customers]
+answer = 0
 
-if sum(customers) > 0:
-    cnt_0 = customers.count(0)
-    customers = [i // fellow for i in customers]
-    answer += sum(customers) + n - cnt_0
+for customer in customers:
+    customer -= leader
+    answer += 1
+
+    if customer > 0:
+        if customer % fellow == 0:
+            answer += customer // fellow
+        else:
+            answer += customer // fellow + 1
 
 print(answer)
