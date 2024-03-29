e08f2ef2e4f2a1b10ab80c595d62a5e5480744ed
psrom <97152657+psrom@users.noreply.github.com>
2024-03-29 18:17:07 +0900

    Title: 바이러스 검사, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240329/바이러스 검사/virus-detector.py
+++ 240329/바이러스 검사/virus-detector.py
@@ -3,7 +3,10 @@ customers = list(map(int, input().split()))
 leader, fellow = map(int, input().split())
 
 answer = n
-customers = [(i - leader) // fellow for i in customers]
-answer += sum(customers) + n
+customers = [i - leader for i in customers]
+
+if sum(customers) > 0:
+    customers = [i // fellow for i in customers]
+    answer += sum(customers) + n
 
 print(answer)
