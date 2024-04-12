b5402121eb0cc739f4a5021ec5a08f334f02af45
psrom <97152657+psrom@users.noreply.github.com>
2024-04-12 18:47:00 +0900

    Title: 2개 중 최대, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240412/2개 중 최대/max-of-two-nums.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, ans;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        ans = a > b ? a : b;
+
+        System.out.println(ans);
+    }
+}
