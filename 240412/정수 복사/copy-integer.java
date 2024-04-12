28681897fd5497beef1bf87e20e0754e670dcf24
psrom <97152657+psrom@users.noreply.github.com>
2024-04-12 15:29:03 +0900

    Title: 정수 복사, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240412/정수 복사/copy-integer.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3, b = 4;
+        b = a;
+        System.out.printf("%d %d\n", a, b);
+        System.out.printf("%d", a * b);
+    }
+}
