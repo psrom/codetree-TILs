8996c1aee0eb6c506179a985a5f4a234cfebff99
psrom <97152657+psrom@users.noreply.github.com>
2024-04-12 15:37:27 +0900

    Title: 데이터 교환, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

--- 240412/데이터 교환/exchange-data.java
+++ 240412/데이터 교환/exchange-data.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int a = 5, b = 6, c = 7;
-        int temp1, temp2;
-        temp1 = b;
+        int temp;
+        temp = b;
         b = a;
-        temp2 = c;
-        c = temp1;
-        a = temp2;
-
+        a = c;
+        c = temp;
+        
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
