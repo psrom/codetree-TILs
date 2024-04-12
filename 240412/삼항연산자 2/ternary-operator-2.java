0b4e7d72d9cf4571ab3d008f3c1c695ac446db8a
psrom <97152657+psrom@users.noreply.github.com>
2024-04-12 18:50:50 +0900

    Title: 삼항연산자 2, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240412/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        String res = a == 1 ? "t": "f";
+        System.out.println(res);
+    }
+}
