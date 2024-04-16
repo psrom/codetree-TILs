152f634a71e7030bc54c0613a1082b15318a7ef2
psrom <97152657+psrom@users.noreply.github.com>
2024-04-16 16:17:49 +0900

    Title: a/b 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240416/a/b 출력/a-divide-b.java
+++ 240416/a/b 출력/a-divide-b.java
@@ -2,19 +2,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(), b = sc.nextInt(), temp;
-        int m = a / b;
-        System.out.print(m + ".");
+        int a = sc.nextInt(), b = sc.nextInt();
+        int integerPart = a / b;
+        int remainder = a % b;
 
-        if (m != 0) {
-            temp = a * 10 / b;
-            a = (a * 10) % b;
-        }
+        System.out.print(integerPart + ".");
 
         for (int i = 0; i < 20; i++) {
-            temp = a * 10 / b;
-            a = (a * 10) % b;
-            System.out.print(temp);
+            remainder *= 10;
+            int digit = remainder / b;
+            remainder %= b;
+            System.out.print(digit);
         }
     }
 }
