66bfedaa1513825cae2f27e4eb17e3950d33d545
psrom <97152657+psrom@users.noreply.github.com>
2024-04-18 21:49:21 +0900

    Title: 청소당번 정하기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240418/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int classroom = 0, passage = 0, toilet = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (i % 12 == 0) {
+                toilet += 1;
+            } else if (i % 3 == 0) {
+                passage += 1;
+            } else if (i % 2 == 0) {
+                classroom += 1;
+            }
+        }
+
+        System.out.printf("%d %d %d", classroom, passage, toilet);
+    }
+}
