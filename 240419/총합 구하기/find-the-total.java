e5d7c755c6c4293f27685ec13a0e2ed93048b565
psrom <97152657+psrom@users.noreply.github.com>
2024-04-19 01:17:15 +0900

    Title: 총합 구하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240419/총합 구하기/find-the-total.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        int sum = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (i % 6  == 0 && i % 8 != 0) {
+                sum += i;
+            }
+        }
+        System.out.print(sum);
+    }
+}
