c60df8b23259a98af687f6109da729a45cdb2819
psrom <97152657+psrom@users.noreply.github.com>
2024-04-20 21:42:46 +0900

    Title: ab 사이에 있는 c, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240420/ab 사이에 있는 c/c-between-a-and-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        boolean check = false;
+
+        for (int i = a; i <= b; i++) {
+            if (i % c == 0) {
+                check = true;
+                break;
+            }
+        }
+
+        if (check == true) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
+    }
+}
