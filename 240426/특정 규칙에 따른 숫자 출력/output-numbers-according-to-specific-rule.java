3d8be3961cd69b4860b15fac5ff8a24eea65f1cb
psrom <97152657+psrom@users.noreply.github.com>
2024-04-26 13:46:55 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240426/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int tmp = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (tmp == 10) {
+                tmp = 1;
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(tmp + " ");
+                tmp++;
+            }
+            System.out.println();
+        }
+    }
+}
