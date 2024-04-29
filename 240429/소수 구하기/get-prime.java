7bb381a93f5b374bcf23f49085a8e6d7fbc0bcaf
psrom <97152657+psrom@users.noreply.github.com>
2024-04-29 16:45:43 +0900

    Title: 소수 구하기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240429/소수 구하기/get-prime.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            boolean check = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check)
+            System.out.print(i + " ");
+        }
+    }
+}
