86393bf9657f6b9cd5041ea1ef23f480bae22d5e
psrom <97152657+psrom@users.noreply.github.com>
2024-05-02 20:56:54 +0900

    Title: 원소 10개의 합, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240502/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum, val;
+        sum = 0;
+
+        for (int i = 0; i < 10; i++) {
+            val = sc.nextInt();
+            sum += val;
+        }
+
+        System.out.println(sum);
+    }
+}
