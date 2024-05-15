44dfab6c04835f997fbf10422a35b37d74a7442a
psrom <97152657+psrom@users.noreply.github.com>
2024-05-15 11:53:47 +0900

    Title: 제곱하여 출력하기, Time: 194ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240515/제곱하여 출력하기/print-square-of-elements.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            int a = arr[i];
+            System.out.printf("%d ", a * a);
+        }
+    }
+}
