cc9c17fea2b67491a011a3bf0f0456777a460a37
psrom <97152657+psrom@users.noreply.github.com>
2024-05-22 16:43:46 +0900

    Title: n개의 숫자 중 최대 2개, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240522/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        final int INT_MIN = Integer.MIN_VALUE;
+        int maxVal1 = INT_MIN, maxVal2 = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            if (a > maxVal1) {
+                maxVal2 = maxVal1;
+                maxVal1 = a;
+                continue;
+            }
+
+            if (a > maxVal2) {
+                maxVal2 = a;
+                continue;
+            }
+        }
+
+        System.out.printf("%d %d", maxVal1, maxVal2);
+    }
+}
