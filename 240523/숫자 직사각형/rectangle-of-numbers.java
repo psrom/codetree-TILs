8e807ef651d300269f46782caf79df3b62434d9c
psrom <97152657+psrom@users.noreply.github.com>
2024-05-23 16:13:18 +0900

    Title: 숫자 직사각형, Time: 438ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240523/숫자 직사각형/rectangle-of-numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        int[][] arr = new int[n][m];
+
+        int cnt = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.printf("%d ", cnt);
+                cnt++;
+            }
+            System.out.println();
+        }
+    }
+}
