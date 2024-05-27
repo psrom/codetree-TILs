214c6d741173b771f23164900b7d55be1a6a5088
psrom <97152657+psrom@users.noreply.github.com>
2024-05-27 14:44:49 +0900

    Title: 문자열의 길이 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240527/문자열의 길이 출력하기/print-string's-range.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a1 = sc.next(), a2 = sc.next();
+
+        System.out.println(a1.length() + a2.length());
+    }
+}
