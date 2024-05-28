78da98b143034c5f1a18f9ec2ad1bd247f48e233
psrom <97152657+psrom@users.noreply.github.com>
2024-05-28 15:19:45 +0900

    Title: 문자 개수 세기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/문자 개수 세기/count-char.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char target = sc.next().charAt(0);
+        int cnt = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == target) {
+                cnt++;
+            }
+        }
+        
+        System.out.println(cnt);
+    }
+}
